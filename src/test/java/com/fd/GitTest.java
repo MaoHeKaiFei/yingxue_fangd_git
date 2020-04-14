13e3486edd26e2dbbecb5c85d7fe30df29d5eef5
fangdie <1544706787@qq.com>
2020-04-14 19:08:28 +0800

    这是应学项目第三次提交 v3.0

--- src/test/java/com/fd/GitTest.java
+++ src/test/java/com/fd/GitTest.java
@@ -1,5 +1,15 @@
 package com.fd;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GitTest {
 
+    @Test
+    public void test1() {
+    }
 }
